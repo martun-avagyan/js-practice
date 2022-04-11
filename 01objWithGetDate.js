b957c575dceaa2a363806d342e6bae17f3e7f2fa
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-04-11 15:13:27 +0400

    feat: create a class with getDate method that gets the date.

--- 01objWithGetDate.js
+++ 01objWithGetDate.js
@@ -5,3 +5,22 @@ const obj = {
     return new Date();
   },
 };
+
+class ObjectWithDate {
+  constructor() {}
+  static getDate() {
+    return new Date();
+  }
+}
+
+const objWithGetDate = new ObjectWithDate();
+
+// objWithGetDate.getDate();
+
+try {
+  objWithGetDate.prototype.getDate();
+} catch (error) {
+  console.log(error);
+}
+
+console.log(ObjectWithDate.getDate());
