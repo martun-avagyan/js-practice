5f19659842625d01ddf9f412495c2ea2c3b40be2
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-04-11 20:33:05 +0400

    feat: map implementation

---
+++ 10map.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const myMapFunction = function (arr, callback) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]));
+  }
+  return newArr;
+};
